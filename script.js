5b1b4aa31a1ac86fc0d1a343b3954514baa82d7c
Sanuji Gamagedara <wimansasanuji@gmail.com>
2026-01-26 13:36:56 +0200

    moved the todo list items to a js array and changed the styling in the webpage

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+let todo = [
+    "task 1",
+    "task 2"
+];
+
 const todoList = document.getElementById("todoList");
 const todoInput = document.getElementById("todoInput");
 const addButton = document.getElementById("addButton");
+
+function renderTodo() {
+    todoList.innerHTML = "";
+
+    todo.forEach((toDo, index) => {
+        const li = document.createElement("li");
+        li.textContent = todo + " ";
+
+        const removeButton = document.createElement("button");
+        removeButton.textContent = '✕';
+        removeButton.className = 'removeButton';
+
+        removeButton.addEventListener("click", function() {
+            removeTodo(index);
+        });
+
+        li.appendChild(removeButton);
+        todoList.appendChild(li);
+    });
+}
+
+function removeTodo(index) {
+    todo.splice(index, 1);
+    renderTodo();
+}
+
 
 todoList.addEventListener("click", function (event) {
     if (event.target.tagName === "LI") {
@@ -19,20 +50,9 @@ addButton.addEventListener("click",function() {
         return;
     }
 
-    const li = document.createElement("li");
-    li.textContent = text + " ";
-
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "remove";
-
-    removeButton.addEventListener("click",function() {
-        todoList.removeChild(li);
-    }
-);
-
-    li.appendChild(removeButton);
-    todoList.appendChild(li);
-
+    todo.push(text);
+    renderTodo();
     todoInput.value = "";
-}
-);
+});
+
+renderTodo();
