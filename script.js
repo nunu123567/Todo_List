e5124ba3024196e72ecddd2fc801c603ebe727e0
Sanuji Gamagedara <wimansasanuji@gmail.com>
2026-01-29 08:44:12 +0200

    changed the js so that the todo list item array store into the web browser memory

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-let todo = [
-    "task 1",
-    "task 2"
-];
+let todo = JSON.parse(localStorage.getItem("todos")) || [];
 
 const todoList = document.getElementById("todoList");
 const todoInput = document.getElementById("todoInput");
 const addButton = document.getElementById("addButton");
+
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todo));
+}
 
 function renderTodo() {
     todoList.innerHTML = "";
 
     todo.forEach((toDo, index) => {
         const li = document.createElement("li");
-        li.textContent = todo + " ";
+        li.textContent = toDo + " ";
 
+        li.addEventListener("click", function () {
+            li.style.textDecoration =
+                li.style.textDecoration === "line-through"
+                    ? "none"
+                    : "line-through";
+        });
+  
         const removeButton = document.createElement("button");
-        removeButton.textContent = '✕';
-        removeButton.className = 'removeButton';
+        removeButton.textContent = "✕";
+        removeButton.className = "removeButton";
 
-        removeButton.addEventListener("click", function() {
+        removeButton.addEventListener("click", function (e) {
+            e.stopPropagation();
             removeTodo(index);
         });
 
@@ -29,30 +38,20 @@ function renderTodo() {
 
 function removeTodo(index) {
     todo.splice(index, 1);
+    saveTodos();
     renderTodo();
 }
 
-
-todoList.addEventListener("click", function (event) {
-    if (event.target.tagName === "LI") {
-        if (event.target.style.textDecoration === "line-through") {
-            event.target.style.textDecoration = "none";
-        } else {
-            event.target.style.textDecoration = "line-through";
-        }
-    }
-});
-
-addButton.addEventListener("click",function() {
+addButton.addEventListener("click", function () {
     const text = todoInput.value.trim();
 
-    if(text === ""){
-        return;
-    }
+    if (text === "") return;
 
     todo.push(text);
+    saveTodos();
     renderTodo();
     todoInput.value = "";
 });
+
 
 renderTodo();
